6e7821694d739a5d60134d979cd74f9b8ce35861
Eric Zivnuska <ezivnuska@gmail.com>
2023-06-26 02:45:32 -0700

    added simple form

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,38 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import {
-    Dimensions,
+    Button,
     StyleSheet,
+    Text,
+    TextInput,
     View,
 } from 'react-native'
-import { Body, Header } from './'
-import { Navigation } from '../navigators'
-
-const windowDimensions = Dimensions.get('window');
-const screenDimensions = Dimensions.get('screen');
 
 const Layout = () => {
-    
-    const [dimensions, setDimensions] = useState({
-        window: windowDimensions,
-        screen: screenDimensions,
-    })
 
-    useEffect(() => {
-        const subscription = Dimensions.addEventListener(
-            'change',
-            ({ window, screen }) => {
-                setDimensions({ window, screen })
-            }
-        )
-        return () => subscription.remove()
-    }, [])
+    const [inputValue, setInputValue] = useState('')
+
+    const submitForm = () => {
+        console.log(inputValue)
+
+        // example of axios request
+
+        // axios.post('/api/data', (req, res) => {
+        //     console.log(res.data)
+        // })
+    }
 
     return (
-        <View style={[styles.layoutContainer, { width: dimensions.window.width }]}>
-            <Header>iamzach</Header>
-            <Body>
-                <Navigation />
-            </Body>
+        <View style={styles.container}>
+        
+            <View style={styles.formContainer}>
+
+                <Text style={styles.header}>Simple Form</Text>
+
+                <Text style={styles.display}>{inputValue}</Text>
+                
+                <TextInput
+                    style={styles.input}
+                    onChangeText={value => setInputValue(value)}
+                    placeholder='write something...'
+                    placeholderTextColor='#ccc'
+                />
+
+                <Button
+                    title='Submit'
+                    onPress={() => submitForm()}
+                />
+
+            </View>
+
         </View>
     )
 }
@@ -40,11 +51,40 @@ const Layout = () => {
 export default Layout
 
 const styles = StyleSheet.create({
-    layoutContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
+    container: {
         height: '100%',
         minHeight: '100%',
+        padding: 20,
+    },
+    formContainer: {
+        padding: 20,
+        borderWidth: 1,
+        borderRadius: 6,
+    },
+    header: {
+        marginBottom: 10,
+        fontSize: 24,
+        fontWeight: 700,
+        lineHeight: 24,
+        color: 'black',
+        height: 24,
+    },
+    display: {
+        marginBottom: 10,
+        fontSize: 20,
+        fontWeight: 700,
+        lineHeight: 20,
+        color: 'green',
+        height: 20,
+    },
+    input: {
+        borderWidth: 1,
+        marginBottom: 10,
+        padding: 10,
+        lineHeight: 24,
+        fontSize: 20,
+    },
+    button: {
+
     },
 })
---
+++ src/layout/LayoutWithNavigation.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import {
+    Dimensions,
+    StyleSheet,
+    View,
+} from 'react-native'
+import { Body, Header } from '.'
+import { Navigation } from '../navigators'
+
+const windowDimensions = Dimensions.get('window');
+const screenDimensions = Dimensions.get('screen');
+
+const Layout = () => {
+    
+    const [dimensions, setDimensions] = useState({
+        window: windowDimensions,
+        screen: screenDimensions,
+    })
+
+    useEffect(() => {
+        const subscription = Dimensions.addEventListener(
+            'change',
+            ({ window, screen }) => {
+                setDimensions({ window, screen })
+            }
+        )
+        return () => subscription.remove()
+    }, [])
+
+    return (
+        <View style={[styles.layoutContainer, { width: dimensions.window.width }]}>
+            <Header>iamzach</Header>
+            <Body>
+                <Navigation />
+            </Body>
+        </View>
+    )
+}
+
+export default Layout
+
+const styles = StyleSheet.create({
+    layoutContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        height: '100%',
+        minHeight: '100%',
+    },
+})
