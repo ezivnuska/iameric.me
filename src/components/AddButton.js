6f662939e3ec3587e93e081c788daf87f43a3c56
Eric Zivnuska <ezivnuska@gmail.com>
2023-09-30 00:02:40 -0700

    clean up

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,26 +1,12 @@
 import React from 'react'
-import {
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-} from 'react-native'
 import { PlusOutlined } from '@ant-design/icons'
 import { Button } from 'antd'
-import main from '../styles/main'
 
-const AddButton = ({ iconStyle, onPress }) => (
-    <TouchableOpacity
-        style={styles.container}
-        onPress={onPress}
-    >
-        <Button size='small' shape='circle' icon={<PlusOutlined style={iconStyle} />} />
-    </TouchableOpacity>
+export default ({ iconStyle, onPress }) => (
+    <Button
+        size='small'
+        shape='circle'
+        icon={<PlusOutlined style={iconStyle} />}
+        onClick={onPress}
+    />
 )
-
-export default AddButton
-
-const styles = StyleSheet.create({
-    container: {
-        marginHorizontal: 7,
-    },
-})
