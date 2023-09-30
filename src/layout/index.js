f32e8204e02a1cb4033321f8d717268783561c65
Eric Zivnuska <ezivnuska@gmail.com>
2023-09-30 00:14:42 -0700

    removed unused Layout components

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,13 +1,7 @@
 import Body from './Body'
-import AnimatedLayout from './AnimatedLayout'
-import FixedLayout from './FixedLayout'
 import Layout from './Layout'
-import ListLayout from './ListLayout'
 
 export {
     Body,
-    AnimatedLayout,
-    FixedLayout,
     Layout,
-    ListLayout,
 }
