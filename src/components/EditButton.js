0af2e054eece5153ebb8019ee11e1e46a7ca9b7c
Eric Zivnuska <ezivnuska@gmail.com>
2023-11-11 16:50:04 -0800

    added webpack bundle analyzer/import plugin for reduced bundle size

--- src/components/EditButton.js
+++ src/components/EditButton.js
@@ -1,26 +1,16 @@
 import React from 'react'
 import {
-    StyleSheet,
-    Text,
     TouchableOpacity,
 } from 'react-native'
 import { EditOutlined } from '@ant-design/icons'
 import { Button } from 'antd'
 import main from '../styles/main'
 
-const EditButton = ({ iconStyles, onPress }) => (
+export default ({ iconStyles, onPress }) => (
     <TouchableOpacity
-        style={[styles.container, main.paddedH]}
+        style={main.paddedH}
         onPress={onPress}
     >
         <Button shape='circle' icon={<EditOutlined style={iconStyles} />} />
     </TouchableOpacity>
 )
-
-export default EditButton
-
-const styles = StyleSheet.create({
-    container: {
-        // paddingBottom: 5, 
-    },
-})
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 const appDirectory = path.resolve(__dirname);
 const {presets} = require(`${appDirectory}/babel.config.js`);
@@ -23,14 +25,21 @@ const babelLoaderConfiguration = {
     path.resolve(__dirname, 'src'),
     ...compileNodeModules,
   ],
-  use: {
+  use: [{
     loader: 'babel-loader',
     options: {
       cacheDirectory: true,
       presets,
-      plugins: ['react-native-web'],
+      plugins: [
+        'react-native-web',
+        [
+          'import',
+          { libraryName: 'antd', style: true },
+          'antd',
+        ],
+      ],
     },
-  },
+  }],
 };
 
 const svgLoaderConfiguration = {
@@ -64,6 +73,33 @@ const fileLoaderConfiguration = {
   },
 }
 
+let plugins = [
+  new HtmlWebpackPlugin({
+    template: path.join(__dirname, 'index.html'),
+  }),
+  // new webpack.HotModuleReplacementPlugin(),
+  new webpack.DefinePlugin({
+    // 'process.env': JSON.stringify(process.env),
+    '__DEV__': isDebug,
+    'process.env': {
+      'NODE_ENV': isDebug ? JSON.stringify('development') : JSON.stringify('production'),
+    },
+  }),
+  new webpack.ProvidePlugin({
+    process: 'process/browser',
+  }),
+  // new CopyPlugin({
+  //   patterns: [
+  //     {
+  //       from: './assets',
+  //       to: './assets',
+  //     }
+  //   ],
+  // }),
+]
+
+if (isDebug) plugins = [...plugins, new BundleAnalyzerPlugin()]
+
 module.exports = {
   entry: {
     app: path.join(__dirname, 'index.web.js'),
@@ -73,6 +109,17 @@ module.exports = {
     publicPath: '/',
     filename: '[name].bundle.js',
     clean: true,
+  },
+  optimization: {
+    minimize: !isDebug,
+    minimizer: [
+      new TerserPlugin(),
+    ],
+    // splitChunks: {
+    //   chunks: 'all',
+    //   minSize: 10000,
+    //   maxSize: 250000,
+    // },
   },
   resolve: {
     // modules: ['node_modules'],
@@ -103,28 +150,8 @@ module.exports = {
       svgLoaderConfiguration,
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'index.html'),
-    }),
-    // new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      // 'process.env': JSON.stringify(process.env),
-      '__DEV__': isDebug,
-    }),
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-    }),
-    // new CopyPlugin({
-    //   patterns: [
-    //     {
-    //       from: './assets',
-    //       to: './assets',
-    //     }
-    //   ],
-    // }),
-  ],
-  devtool: 'source-map',
+  plugins,
+  devtool: isDebug ? 'eval' : 'source-map',
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'assets'),
