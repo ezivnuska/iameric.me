0c2ee83d4c1fba240c6503373ad43f583ed80512
Eric Zivnuska <ezivnuska@gmail.com>
2024-05-19 04:11:01 -0700

    bug in Forum migration

--- src/components/Forum/components/ForumList/components/ForumListItem.js
+++ src/components/Forum/components/ForumList/components/ForumListItem.js
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {
-    FlatList,
     Image,
     Pressable,
-    ScrollView,
     View,
 } from 'react-native'
 import {
@@ -11,15 +9,11 @@ import {
     ThemedText,
 } from '@components'
 import classes from '@styles/classes'
-import { getProfileImagePathFromUser } from '@utils/images'
 import {
     useApp,
-    useForum,
-    useModal,
-    useUser,
 } from '@context'
 
-const ListItemVertical = ({ item, imagePath, onPress, owner, onDelete = null, ...props }) => {
+export default ({ item, imagePath, onPress, owner, onDelete = null, ...props }) => {
     
     const { theme } = useApp()
     
@@ -114,7 +108,6 @@ const ListItemVertical = ({ item, imagePath, onPress, owner, onDelete = null, ..
                 <ThemedText
                     style={{
                         flex: 1,
-                        // paddingRight: 30,
                         paddingBottom: 10,
                     }}
                 >
@@ -125,184 +118,4 @@ const ListItemVertical = ({ item, imagePath, onPress, owner, onDelete = null, ..
 
         </View>
     )
-}
-
-const ListItemHorizontal = ({ item, imagePath, onPress, owner, onDelete = null }) => {
-    const { author, text } = item
-    const [online, setOnline] = useState(false)
-    const { dims, theme } = useApp()
-    
-    useEffect(() => {
-        if (author && author.exp) {
-            const newDate = new Date(author.exp) - Date.now()
-            const expired = (newDate > 0)
-            setOnline(!expired)
-        }
-    }, [item])
-
-    return (
-        <ScrollView
-            showsVerticalScrollIndicator={false}
-            style={{
-                width: '100%',
-                maxWidth: 600,
-                minWidth: '50%',
-            }}
-        >
-            <View
-                style={{
-                    width: '100%',
-                    maxWidth: 600,
-                    flexGrow: 1,
-                    flexDirection: 'row',
-                    justifyContent: 'flex-start',
-                    alignItems: 'flex-start',
-                    gap: 15,
-                    paddingHorizontal: 10,
-                    marginTop: dims.height < 400 ? '5%' : dims.height < 600 ? '15%' : '33%',
-                }}
-            >
-                <View
-                    style={{
-                        flex: 1,
-                        flexBasis: 'auto',
-                        flexGrow: 0,
-                        flexShrink: 0,
-                    }}
-                >
-                    
-                    <Image
-                        style={{
-                            width: 70,
-                            height: 70,
-                        }}
-                        source={imagePath}
-                    />
-                </View>
-
-                <View
-                    style={{
-                        flex: 1,
-                        flexGrow: 1,
-                    }}
-                >
-                
-                    <View
-                        style={{
-                            flexBasis: 'auto',
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            paddingBottom: 5,
-                            borderBottomWidth: 1,
-                            borderBottomStyle: 'dotted',
-                            borderBottomColor: theme?.colors.border,
-                        }}
-                    >
-                        <Pressable
-                            style={{ flexBasis: 'auto' }}
-                            onPress={onPress}
-                        >
-                            <ThemedText
-                                style={classes.userHeading}
-                            >
-                                {author.username}
-                                {online && <ThunderboltOutlined style={{ marginLeft: 10, color: 'green' }} />}
-                            </ThemedText>
-
-                        </Pressable>
-
-                        {(owner && onDelete) && (
-                            <View
-                                style={{
-                                    flexBasis: 'auto',
-                                    flexGrow: 0,
-                                    marginTop: -1,
-                                }}
-                            >
-                                <IconButton
-                                    iconName='trash-outline'
-                                    onPress={() => onDelete(item._id)}
-                                    textStyles={{ fontSize: 15 }}
-                                    transparent
-                                />
-                            </View>
-                        )}
-
-                    </View>
-                    
-                    <ThemedText
-                        style={{
-                            marginTop: 10,
-                            textAlign: 'left',
-                            // paddingBottom: 20,
-                        }}
-                    >
-                        {text}
-                    </ThemedText>
-                </View>
-            </View>
-
-        </ScrollView>
-    )
-}
-
-export default ({ entries, onDelete }) => {
-    const { landscape, dims } = useApp()
-    const { setModal } = useModal()
-    const { profile } = useUser()
-
-    return !landscape ? (
-        <FlatList
-            data={entries}
-            listKey={() => 'entries'}
-            keyExtractor={(item, index) => `${index}-entry-${item._id}`}
-            renderItem={({ item }) => (
-                <ListItemVertical
-                    item={item}
-                    imagePath={getProfileImagePathFromUser(item.author)}
-                    onDelete={onDelete}
-                    onPress={() => setModal('CONTACT', item.author)}
-                    owner={profile && profile._id === item.author._id}
-                />
-            )}
-            showsVerticalScrollIndicator={false}
-            contentContainerStyle={{
-                paddingBottom: 20,
-            }}
-        />
-    ) : (
-        <ScrollView
-            showsHorizontalScrollIndicator={false}
-            horizontal={true}
-            style={{
-                height: dims.height - 150,
-                margin: 0,
-                width: '100%',
-            }}
-            contentContainerStyle={{
-                maxWidth: 900,
-                marginHorizontal: 'auto',
-            }}
-        >
-            <View
-                style={{
-                    width: '100%',
-                    flexDirection: 'row',
-                    justifyContent: 'flex-start',
-
-                }}
-            >
-                {entries.map(((item, index) => (
-                    <ListItemHorizontal
-                        item={item}
-                        imagePath={getProfileImagePathFromUser(item.author)}
-                        key={'entry' + index}
-                        onDelete={onDelete}
-                        onPress={() => setModal('CONTACT', item.author)}
-                        owner={profile && profile._id === item.author._id}
-                    />
-                )))}
-            </View>
-        </ScrollView>
-    ) 
 }
