117fbfaba41eeabc56ffa4b0f3e2cce97d104241
Eric Zivnuska <ezivnuska@gmail.com>
2024-07-07 02:03:04 -0700

    added header to profile screen with options menu button

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState } from 'react'
+import {
+    Pressable,
+    View,
+} from 'react-native'
 import {
     Screen,
     SimpleButton,
@@ -7,13 +10,49 @@ import {
 } from '@components'
 import { useApp } from '@app'
 import { useModal } from '@modal'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 export default props => {
     // console.log('check out available props from navigation container...', props)
     // we are using the navigate method below.
 
-    const { user } = useApp()
+    const { theme, user } = useApp()
     const { setModal } = useModal()
+
+    const [optionsVisible, setOptionsVisible] = useState(false)
+
+    const renderHeader = () => (
+        <View
+            style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignContent: 'center',
+            }}
+        >
+            <ThemedText bold size={20}>
+                {user && user.username}
+            </ThemedText>
+
+            <Pressable
+                onPress={() => setOptionsVisible(!optionsVisible)}
+                style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: 5,
+                }}
+            >
+                <ThemedText size={16}>
+                    {`${optionsVisible ? 'hide' : 'show'} options`}
+                </ThemedText>
+
+                <Icon
+                    name={optionsVisible ? 'chevron-up' : 'chevron-down'}
+                    size={18}
+                    color={theme?.colors.textDefault}
+                />
+            </Pressable>
+        </View>
+    )
     
     return (
         <Screen
@@ -30,14 +69,19 @@ export default props => {
 
                 <View style={{ flexGrow: 1 }}>
                     
-                    <ThemedText>
-                        {user && user.username}
-                    </ThemedText>
+                    {renderHeader()}
                     
-                    <SimpleButton
-                        label='Close Account'
-                        onPress={() => setModal('DESTROY')}
-                    />
+                    <View
+                        style={{
+                            display: optionsVisible ? 'flex' : 'none',
+                            paddingVertical: 10,
+                        }}
+                    >
+                        <SimpleButton
+                            label='Close Account'
+                            onPress={() => setModal('DESTROY')}
+                        />
+                    </View>
 
                 </View>
 
