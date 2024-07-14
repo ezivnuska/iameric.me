916dea811b4d818fc26d5ffe1b0a4271b401189f
Eric Zivnuska <ezivnuska@gmail.com>
2024-07-14 01:57:06 -0700

    added Contact module for future deployment

---
+++ src/modules/Contact/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { View } from 'react-native'
+
+export default contact => {
+    
+    return (
+
+        <View
+            style={{
+                flexGrow: 1,
+                justifyContent: 'space-between',
+                gap: 20,
+            }}
+        >
+            <View style={{ flexGrow: 1 }}>
+                {contact.username}    
+            </View>
+
+        </View>
+    )
+}
