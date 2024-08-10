631893922a162d626176cbd7c1b787b4ae53dd2c
Eric Zivnuska <ezivnuska@gmail.com>
2024-08-09 21:44:22 -0700

    minor map bugs

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -4,23 +4,20 @@ import { Screen } from './components'
 import { Heading } from '@components'
 import { Map } from '@modules'
 
-export default props => {
+export default props => (
+    <Screen {...props}>
 
-    return (
-        <Screen {...props}>
+        <View style={{ flex: 1 }}>
 
-            <View style={{ flex: 1 }}>
-
-                <Heading
-                    title='Map'
-                    onBack={() => props.navigation.navigate('User', { screen: 'Profile' })}
-                />
-                
-                <View style={{ flexGrow: 1 }}>
-                    <Map />
-                </View>
+            <Heading
+                title='Map'
+                onBack={() => props.navigation.navigate('User', { screen: 'Profile' })}
+            />
+            
+            <View style={{ flexGrow: 1 }}>
+                <Map />
             </View>
+        </View>
 
-        </Screen>
-    )
-}
+    </Screen>
+)
