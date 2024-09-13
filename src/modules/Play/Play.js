b8c97a1ef7c7c474c33dff050013d941aa40b2a9
Eric Zivnuska <ezivnuska@gmail.com>
2024-09-13 16:27:23 -0700

    added tile game

--- src/modules/Play/Play.js
+++ src/modules/Play/Play.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { View } from 'react-native'
 import { FocusView } from '@components'
+import { Puzzle } from './components'
 
 export default () => (
     <View style={{ flex: 1 }}>
-        <FocusView />
+        {/* <FocusView /> */}
+        <Puzzle />
     </View>
 )
