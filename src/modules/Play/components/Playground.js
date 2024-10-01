93025774d06dc02824cf22930f43861cfbe3193b
Eric Zivnuska <ezivnuska@gmail.com>
2024-10-01 04:40:54 -0700

    more puzzle work

--- src/modules/Play/components/Playground.js
+++ src/modules/Play/components/Playground.js
@@ -2,10 +2,11 @@ import 'react-native-gesture-handler'
 import React, { useEffect, useMemo, useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import Animated, {
+	Easing,
 	ReduceMotion,
 	useAnimatedStyle,
 	useSharedValue,
-	withDecay,
+	// withDecay,
 	withSpring,
 	withTiming,
 } from 'react-native-reanimated'
@@ -14,6 +15,7 @@ import {
 	GestureDetector,
 	GestureHandlerRootView,
 } from 'react-native-gesture-handler'
+import { ThemedText } from '@components'
 
 export default ({ level }) => {
 
@@ -21,13 +23,6 @@ export default ({ level }) => {
 
 	const [tiles, setTiles] = useState([])
 	const [availableWidth, setAvailableWidth] = useState(null)
-
-	// const emptyIndex = useSharedValue(level * level - 1)
-
-	// const [ moving, setMoving ] = useState(false)
-	// const [ updating, setUpdating ] = useState(false)
-	// const [ empty, setEmpty ] = useState({ emptyCol: level - 1, emptyRow: level - 1 })
-	// const [ movableTiles, setMovableTiles ] = useState([])
 
 	const offsetX = useSharedValue(0)
 	const offsetY = useSharedValue(0)
@@ -46,29 +41,7 @@ export default ({ level }) => {
 		}
 	}, [itemSize])
 
-	// useEffect(() => {
-	// 	if (!moving) {
-	// 		offsetX.value = 0
-	// 		offsetY.value = 0
-	// 		console.log('moving done')
-	// 	}
-	// }, [moving])
-	
-	// useEffect(() => {
-	// 	setItemSize(availableWidth / level)
-	// }, [])
-
-	// useEffect(() => {
-	// 	setEmpty({ emptyCol: level - 1, emptyRow: level - 1 })
-	// }, [])
-
-	// useEffect(() => {
-	// 	// if (empty) initTiles()
-	// 	console.log('empty', empty)
-	// }, [empty])
-
 	useEffect(() => {
-		console.log('tiles', tiles)
 		offsetX.value = 0
 		offsetY.value = 0
 	}, [tiles])
@@ -107,21 +80,12 @@ export default ({ level }) => {
 
 	const { emptyCol, emptyRow } = useMemo(() => {
 		if (!tiles.length) return {}
-		// if (moving) return {}
 		const emptyCol = getEmptyCol()
 		const emptyRow = getEmptyRow()
-		console.log('*empty', emptyRow, emptyCol)
 		return { emptyCol, emptyRow }
 	}, [tiles])
 
-	// useEffect(() => {
-	// 	// console.log('current', current)
-	// 	const extraTile = getExtraTile(current)
-	// 	setExtra(extraTile)
-	// }, [current])
-
 	const initTiles = () => {
-		console.log('init tiles (once)')
 		const array = []
 		while (array.length < level * level - 1) {
 			const col = array.length % level
@@ -136,9 +100,6 @@ export default ({ level }) => {
 	}
 	
 	const tileIsDisabled = tile => {
-		console.log('tileIsDisabled')
-		console.log('empty', emptyRow, emptyCol)
-		console.log('tile', tile.row, tile.col)
 		return !(emptyCol === tile.col || emptyRow === tile.row)
 	}
 
@@ -169,8 +130,6 @@ export default ({ level }) => {
 
 	const getExtraTile = id => {
 		const { direction, distance } = getDirectionAndDistance(id)
-		console.log('direction', direction)
-		console.log('distance', distance)
 		const { col, row } = getColRowFromId(id)
 		
 		let extra = null
@@ -184,27 +143,23 @@ export default ({ level }) => {
 				extraRow = row
 				extraCol = direction === 'left' ? col - 1 : col + 1
 			}
-			console.log('xtra', extraRow, extraCol)
+			
 			tiles.map(t => {
 				if (t.col === extraCol && t.row === extraRow) {
 					extra = t
 				}
 			})
 		}
-		console.log('extra', extra)
+		
 		return extra
 	}
 
 	const onTouchStart = event => {
-		console.log('START')
 		const { id } = event.target.offsetParent
-		console.log('touched', id)
-		console.log('')
 		const { direction, distance } = getDirectionAndDistance(id)
 		let extra = null
 		if (distance === 2) {
 			extra = getExtraTile(id)
-			console.log('extra', extra)
 		}
 
 		const updatedTiles = tiles.map(t => {
@@ -226,27 +181,23 @@ export default ({ level }) => {
 		const { direction } = getDirectionAndDistance(id)
 		if (directionIsVertical(direction)) {
 			if (direction === 'up') {
-				if (translationY >= -itemSize && translationY <= 0) offsetY.value += changeY//translationY
+				if (translationY >= -itemSize && translationY <= 0) offsetY.value = translationY//changeY//
 			} else {
-				if (translationY <= itemSize && translationY >= 0) offsetY.value += changeY//translationY
+				if (translationY <= itemSize && translationY >= 0) offsetY.value = translationY//changeY//
 			}
 		} else {
 			if (direction === 'left') {
-				if (translationX >= -itemSize && translationX <= 0) offsetX.value += changeX//translationX
+				if (translationX >= -itemSize && translationX <= 0) offsetX.value = translationX//changeX//
 			} else {
-				if (translationX <= itemSize && translationX >= 0) offsetX.value += changeX//translationX
+				if (translationX <= itemSize && translationX >= 0) offsetX.value = translationX//changeX//
 			}
 		}
 	}
 
 	const updateTiles = event => {
-		// setMoving(false)
-		// setUpdating(true)
 		const { target } = event
 		const { id } = target.offsetParent
 		const { direction } = getDirectionAndDistance(id)
-		// const { col, row } = getColRowFromId(id)
-		// const newEmptyPos = { emptyCol: col, emptyRow: row }
 		const updatedTiles = tiles.map(t => {
 			if (t.dragging) {
 				let newCol = t.col
@@ -272,26 +223,7 @@ export default ({ level }) => {
 		setTiles(updatedTiles)
 		offsetX.value = 0
 		offsetY.value = 0
-		// setUpdating(false)
 	}
-
-	// const swapTiles = event => {
-	// 	console.log('SWIPE')
-	// 	const { target } = event
-	// 	const { id } = target.offsetParent
-	// 	const { direction } = getDirectionAndDistance(id)
-	// 	if (directionIsVertical(direction)) {
-	// 		offsetY.value = direction === 'up'
-	// 			? -itemSize
-	// 			: itemSize
-	// 	} else {
-	// 		offsetX.value = direction === 'left'
-	// 			? -itemSize
-	// 			: itemSize
-	// 	}
-		
-	// 	moveTiles(event)
-	// }
 
 	const getNewOffset = event => {
 		const { target, translationX, translationY, velocityX, velocityY } = event
@@ -314,17 +246,26 @@ export default ({ level }) => {
 		}
 	}
 
-	const onTouchEnd = (event) => {
-		console.log('END')
+	const onTouchEnd = event => {
 		const { target, translationX, translationY, velocityX, velocityY } = event
 		const { id } = target.offsetParent
 		const { direction } = getDirectionAndDistance(id)
 		let moveThreshold = itemSize / 2
 		if (velocityY > 1000 || velocityX > 1000) {
 			const { x, y } = getNewOffset(event)
-			if (directionIsVertical(direction)) offsetY.value = y
-			else offsetX.value = x
-			updateTiles(event)
+			if (directionIsVertical(direction)) {
+				offsetY.value = withTiming(y, {
+					duration: 50,
+					easing: Easing.in(Easing.quad),
+					reduceMotion: ReduceMotion.System,
+				}, () => updateTiles(event))
+			} else {
+				offsetX.value = withTiming(x, {
+					duration: 50,
+					easing: Easing.in(Easing.quad),
+					reduceMotion: ReduceMotion.System,
+				}, () => updateTiles(event))
+			}
 		} else {
 			const { x, y } = getNewOffset(event)
 			// const clamp = (direction === 'up' || direction === 'left')
@@ -333,13 +274,9 @@ export default ({ level }) => {
 			
 			if (directionIsVertical(direction)) {
 				if (Math.abs(translationY) > moveThreshold) {
-					offsetY.value = withSpring(y, {
-						mass: 1,
-						damping: 10,
-						stiffness: 100,
-						overshootClamping: false,
-						restDisplacementThreshold: 0.01,
-						restSpeedThreshold: 2,
+					offsetY.value = withTiming(y, {
+						duration: 50,
+						easing: Easing.out(Easing.quad),
 						reduceMotion: ReduceMotion.System,
 					  }, () => updateTiles(event))
 					// offsetY.value = withDecay({
@@ -356,6 +293,7 @@ export default ({ level }) => {
 						restDisplacementThreshold: 0.01,
 						restSpeedThreshold: 2,
 						reduceMotion: ReduceMotion.System,
+						duration: 250,
 					  }, clearDrag)
 					// offsetY.value = withDecay({
 					// 	velocity: velocityY,
@@ -365,15 +303,11 @@ export default ({ level }) => {
 				}
 			} else {
 				if (Math.abs(translationX) > moveThreshold) {
-					offsetX.value = withSpring(x, {
-						mass: 1,
-						damping: 10,
-						stiffness: 100,
-						overshootClamping: false,
-						restDisplacementThreshold: 0.01,
-						restSpeedThreshold: 2,
+					offsetX.value = withTiming(x, {
+						duration: 50,
+						easing: Easing.out(Easing.quad),
 						reduceMotion: ReduceMotion.System,
-					  }, () => updateTiles(event))
+					}, () => updateTiles(event))
 					
 				} else {
 					offsetX.value = withSpring(0, {
@@ -384,6 +318,7 @@ export default ({ level }) => {
 						restDisplacementThreshold: 0.01,
 						restSpeedThreshold: 2,
 						reduceMotion: ReduceMotion.System,
+						duration: 250,
 					  }, clearDrag)
 				}
 			}
@@ -391,12 +326,10 @@ export default ({ level }) => {
 	}
 
 	const clearDrag = () => {
-		console.log('CLEAR')
 		setTiles(tiles.map(t => ({
 			...t,
 			dragging: null,
 		})))
-		// setMoving(false)
 	}
 
 	const directionIsVertical = direction => direction === 'up' || direction === 'down'
@@ -407,13 +340,6 @@ export default ({ level }) => {
 		return { col, row }
 	}
 
-	const pan = Gesture.Pan()
-		.runOnJS(true)
-		.onBegin(onTouchStart)
-		.onChange(onTouchMove)
-		// .onEnd(onTouchEnd)
-		.onFinalize(onTouchEnd)
-
 
 	const animatedStyles = useAnimatedStyle(() => ({
 		transform: [
@@ -423,10 +349,66 @@ export default ({ level }) => {
 		],
 	}))
 
+	const renderDisabledSquare = tile => (
+		<View
+			key={`tile-${tile.id}`}
+			style={{
+				position: 'absolute',
+				top: tile.row * itemSize,
+				left: tile.col * itemSize,
+				height: itemSize,
+				width: itemSize,
+				borderWidth: 1,
+				borderColor: '#fff',
+				overflow: 'hidden',
+				borderRadius: 8,
+				cursor: 'default',
+				backgroundColor: '#b58df1',
+			}}
+		>
+			<ThemedText
+				bold
+				color='#FFE04B'
+				align='center'
+				size={48}
+				style={{
+					flex: 1,
+					lineHeight: itemSize,
+				}}
+			>
+				{tile.id + 1}
+			</ThemedText>
+		</View>
+	)
+
+	const renderMovableSquare = tile => (
+		<View
+			style={{
+				flex: 1,
+				width: '100%',
+				borderWidth: 1,
+				borderStyle: 'dotted',
+			}}
+		>
+			<ThemedText
+				bold
+				color='#FFE04B'
+				align='center'
+				size={48}
+				style={{
+					flex: 1,
+					lineHeight: itemSize,
+				}}
+			>
+				{tile.id + 1}
+			</ThemedText>
+		</View>
+	)
+
 	const renderTile = tile => {
 
 		const disabled = tileIsDisabled(tile)
-		console.log('disabled', disabled, tile)
+		
 		const tileStyles = {
 			position: 'absolute',
 			top: tile.row * itemSize,
@@ -440,35 +422,40 @@ export default ({ level }) => {
 			cursor: 'default',
 		}
 
-
 		const pan = Gesture.Pan()
 			.runOnJS(true)
 			.onBegin(onTouchStart)
 			.onChange(onTouchMove)
-			// .onEnd(onTouchEnd)
 			.onFinalize(onTouchEnd)
 		
-		return disabled ? (
-			<View
-				key={`tile-${tile.id}`}
-				style={[
-					tileStyles,
-					{ backgroundColor: '#b58df1' },
-				]}
-			/>
-		) : (
+		return disabled ? renderDisabledSquare(tile)
+		// (
+			// <View
+			// 	key={`tile-${tile.id}`}
+			// 	style={[
+			// 		tileStyles,
+			// 		{ backgroundColor: '#b58df1' },
+			// 	]}
+			// />
+		// ) 
+		: (
 			<Animated.View
 				id={tile.id}
 				key={`tile-${tile.id}`}
-				// onTouchStart={onTouchStart}
-				// onTouchMove={onTouchMove}
-				// onSwipe={onSwipe}
-				// onTouchEnd={onTouchEnd}
 				style={[
-					tileStyles,
+					// tileStyles,
 					{
+						position: 'absolute',
+						top: tile.row * itemSize,
+						left: tile.col * itemSize,
+						height: itemSize,
+						width: itemSize,
+						borderWidth: 1,
+						borderColor: '#fff',
+						overflow: 'hidden',
+						borderRadius: 8,
 						cursor: 'grab',
-						backgroundColor: '#FFE04B',
+						backgroundColor: '#b58df1',
 					},
 					tile.dragging ? animatedStyles : null,
 				]}
@@ -476,24 +463,35 @@ export default ({ level }) => {
 				<GestureDetector
 					gesture={pan}
 				>
-					<View
+					{renderMovableSquare(tile)}
+					{/* <View
 						style={{
 							flex: 1,
 							width: '100%',
 							borderWidth: 1,
 							borderStyle: 'dotted',
 						}}
-					/>
+					>
+						<Text
+							style={{
+								width: itemSize,
+								lineHeight: itemSize,
+								fontSize: 30,
+								textAlign: 'center',
+							}}
+						>
+							{tile.id + 1}
+						</Text>
+					</View> */}
 				</GestureDetector>
 			</Animated.View>
 		)
 	}
 
 	const renderTiles = () => {
-		console.log('rendering tiles...')
-		return tiles.map(item => {
-			return renderTile(item)
-		})
+		// offsetX.value = 0
+		// offsetY.value = 0
+		return tiles.map(item => renderTile(item))
 	}
 
 	return (
@@ -518,7 +516,6 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
-		// height: '100%',
 		width: '100%',
 	},
 })
