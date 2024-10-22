8b809329f8fd3da0bdff9ed0a10f4f1d8b4a9e5c
Eric Zivnuska <ezivnuska@gmail.com>
2024-10-21 21:41:42 -0700

    cleaned up simple example.

--- src/modules/Simple/Simple.js
+++ src/modules/Simple/Simple.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {
-    Button,
+    Pressable,
     StyleSheet,
     Text,
     TextInput,
@@ -37,30 +37,54 @@ export default Simple = () => {
     }
 
     // loop through saved values and return list item as jsx element
-    const renderSavedValues = () => (
-        <View
-            style={styles.valueList}
-        >
-            {savedValues.map((value, index) => (
-                <Text
-                    key={`saved-value-${index}`}
-                    style={styles.valueListItem}
-                >
-                    {value}
+    const renderValueList = () => {
+        return (
+            <View style={styles.valueListContainer}>
+                
+                <Text style={styles.header}>
+                    Comments {savedValues.length ? `(${savedValues.length})` : ''}
                 </Text>
-            ))}
-        </View>
-    )
+
+                <View style={styles.valueList}>
+                    
+                    {savedValues.length ? savedValues.map((value, index) => (
+                        <Text
+                            key={`saved-value-${index}`}
+                            style={styles.valueListItem}
+                        >
+                            {value}
+                        </Text>
+                    )) : (
+                        <Text style={styles.valueListItem}>No comments yet.</Text>
+                    )}
+
+                </View>
+                    
+                {(savedValues.length > 0) && (
+                    <Pressable
+                        onPress={() => setSavedValues([])}
+                        style={[
+                            styles.button,
+                            styles.buttonEnabled,
+                            { marginBottom: 0 },
+                        ]}
+                    >
+                        <Text style={[styles.buttonLabel, styles.buttonLabelEnabled]}>
+                            Clear Comments
+                        </Text>
+                    </Pressable>
+                )}
+
+            </View>
+        )
+    }
 
     return (
-        <View
-            style={styles.container}
-        >
+        <View style={styles.container}>
+
             {/* header */}
-            <Text
-                style={styles.header}
-            >
-                RN TextInput Example
+            <Text style={styles.header}>
+                Comment List Example
             </Text>
 
             {/* input field */}
@@ -73,21 +97,32 @@ export default Simple = () => {
             />
 
             {/* text value preview */}
-            <Text
-                style={styles.textPreview}
-            >
+            <Text style={styles.textPreview}>
                 {textValue}
             </Text>
 
             {/* button to init save */}
-            <Button
-                title='Press Me'
-                color='#f00'
+            <Pressable
                 onPress={handlePress}
-            />
+                disabled={!textValue.length}
+                style={[
+                    styles.button,
+                    textValue.length ? styles.buttonEnabled : styles.buttonDisabled,
+                ]}
+            >
+                <Text
+                    style={[
+                        styles.buttonLabel,
+                        textValue.length ? styles.buttonLabelEnabled : styles.buttonLabelDisabled,
+                    ]}
+                >
+                    Save Comment
+                </Text>
+
+            </Pressable>
 
             {/* render saved values in value list */}
-            {savedValues.length > 0 && renderSavedValues()}
+            {renderValueList()}
 
         </View>
     )
@@ -97,37 +132,69 @@ const styles = StyleSheet.create({
     container: {
         padding: 10,
         backgroundColor: '#ccc',
+        borderRadius: 6,
+        overflow: 'hidden',
     },
     header: {
         paddingBottom: 10,
         fontSize: 20,
-        fonrWeight: 700,
-        color: '#f00',
+        fontWeight: 700,
+        color: '#333',
     },
     textInput: {
         paddingVertical: 4,
         paddingHorizontal: 10,
-        height: 30,
-        lineHeight: 22,
+        marginBottom: 5,
+        height: 40,
+        lineHeight: 40,
         fontSize: 18,
         backgroundColor: '#fff',
+        borderRadius: 6,
+        overflow: 'hidden',
     },
     textPreview: {
-        paddingVertical: 3,
+        paddingHorizontal: 10,
+        marginBottom: 5,
+        height: 26,
         minHeight: 26,
-        lineHeight: 20,
+        lineHeight: 26,
         fontSize: 16,
     },
     button: {
         marginBottom: 10,
+        borderRadius: 6,
+        overflow: 'hidden',
+    },
+    buttonEnabled: {
+        backgroundColor: '#6c6',
+    },
+    buttonDisabled: {
+        backgroundColor: '#eee',
+        textColor: '#ccc',
+    },
+    buttonLabel: {
+        marginHorizontal: 'auto',
+        height: 34,
+        lineHeight: 34,
+        fontSize: 18,
+    },
+    buttonLabelEnabled: {
+        color: '#fff',
+    },
+    buttonLabelDisabled: {
+        color: '#aaa',
+    },
+    valueListContainer: {
+        marginBottom: 7,
     },
     valueList: {
-        marginTop: 7,
+        marginBottom: 7,
     },
     valueListItem: {
-        paddingVertical: 3,
-        minHeight: 26,
+        fontSize: 18,
         lineHeight: 20,
-        fontSize: 16,
+        minHeight: 26,
+        paddingHorizontal: 10,
+        paddingBottom: 5,
     },
 })
