97e3d7968fbf6e3e10df02828951cd2c2d3065de
Eric Zivnuska <ezivnuska@gmail.com>
2024-10-21 19:11:53 -0700

    added comments to simple textinput example

--- src/modules/Simple/Simple.js
+++ src/modules/Simple/Simple.js
@@ -9,15 +9,34 @@ import {
 
 export default Simple = () => {
 
+    // create string var to store textinput value in state
     const [textValue, setTextValue] = useState('')
+
+    // create array to store saved values in state
     const [savedValues, setSavedValues] = useState([])
 
+    // method to handle button press
     const handlePress = () => {
         console.log('button pressed', textValue)
+        
+        // add textinput value to saveValues array
+        // 
+        // note:
+        // this is where we would call an asynchronous request 
+        // to save the value to our database, which will return
+        // confirmation, at which point we can add the item to 
+        // the array of saved values. This way we don't have to 
+        // repeatedly fetch all saved items every time we save a
+        // new value (but that is also an option).
+        // 
+        // for now we'll just save it to the stat array...
         setSavedValues([textValue, ...savedValues])
+
+        // clear text values
         setTextValue('')
     }
 
+    // loop through saved values and return list item as jsx element
     const renderSavedValues = () => (
         <View
             style={styles.valueList}
@@ -37,6 +56,14 @@ export default Simple = () => {
         <View
             style={styles.container}
         >
+            {/* header */}
+            <Text
+                style={styles.header}
+            >
+                RN TextInput Example
+            </Text>
+
+            {/* input field */}
             <TextInput
                 value={textValue}
                 onChangeText={setTextValue}
@@ -45,19 +72,23 @@ export default Simple = () => {
                 style={styles.textInput}
             />
 
+            {/* text value preview */}
             <Text
                 style={styles.textPreview}
             >
                 {textValue}
             </Text>
 
+            {/* button to init save */}
             <Button
                 title='Press Me'
-                color='red'
+                color='#f00'
                 onPress={handlePress}
             />
 
+            {/* render saved values in value list */}
             {savedValues.length > 0 && renderSavedValues()}
+
         </View>
     )
 }
@@ -66,6 +97,12 @@ const styles = StyleSheet.create({
     container: {
         padding: 10,
         backgroundColor: '#ccc',
+    },
+    header: {
+        paddingBottom: 10,
+        fontSize: 20,
+        fonrWeight: 700,
+        color: '#f00',
     },
     textInput: {
         paddingVertical: 4,
