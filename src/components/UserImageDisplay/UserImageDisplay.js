3ccdf75f66b083360b7a07f6df7eff56d35e53cd
Eric Zivnuska <ezivnuska@gmail.com>
2024-11-15 23:55:35 -0800

    updates to user image display control panel visibility

--- src/components/UserImageDisplay/UserImageDisplay.js
+++ src/components/UserImageDisplay/UserImageDisplay.js
@@ -4,16 +4,14 @@ import {
     ActivityIndicator,
     IconButtonLarge,
     ImageContainer,
+    ThemedText,
 } from '@components'
 import { ImageControlPanel } from './components'
 import { loadImage } from '@utils/images'
 import { useImages } from '@images'
 import Animated, {
-	Easing,
 	useAnimatedStyle,
 	useSharedValue,
-	// withDecay,
-	withSpring,
 	withTiming,
 } from 'react-native-reanimated'
 
@@ -26,7 +24,7 @@ const UserImageDisplay = ({ data }) => {
 
     const [image, setImage] = useState(null)
 
-    const [controlsVisible, setControlsVisible] = useState(null)
+    const [controlsVisible, setControlsVisible] = useState(true)
 
     const controlOpacity = useSharedValue(1)
 
@@ -45,67 +43,75 @@ const UserImageDisplay = ({ data }) => {
         if (data) init(data._id)
     }, [])
 
-    useEffect(() => {
-        if (controlsVisible !== null) controlOpacity.value = withTiming(controlsVisible ? 0 : 1, { duration: 500 })
-    }, [controlsVisible])
-
-    // const animate = value => {
-    //     Animated.timing(opacity, {
-    //         toValue: value,
-    //         duration: 3000,
-    //         useNativeDriver: true,
-    //     }).start()
-    // }
-
     const handleControls = () => {
-        setControlsVisible(!controlsVisible)
+        if (controlsVisible) {
+            controlOpacity.value = withTiming(0, { duration: 500 }, () => setControlsVisible(false))
+        } else {
+            setControlsVisible(true)
+            controlOpacity.value = withTiming(1, { duration: 500 })
+        }
     }
 
-    return image ? (
+    return (
         <View
             style={{
                 flex: 1,
                 position: 'relative',
             }}
         >
-            <Pressable
-                style={{ flex: 1 }}
-                onPress={handleControls}
-            >
-                <ImageContainer image={image} />
-            </Pressable>
-            
-            <Animated.View
-                style={[{
-                    position: 'absolute',
-                    top: 10,
-                    right: 10,
-                }, animatedStyle]}
-            >
-                <IconButtonLarge
-                    name='close'
-                    onPress={closeImagesModal}
-                    size={40}
-                    color='tomato'
-                    transparent
-                />
-            </Animated.View>
+            {image ? (
+                <View
+                    style={{
+                        flex: 1,
+                        position: 'relative',
+                    }}
+                >
+                    <Pressable
+                        style={{ flex: 1 }}
+                        onPress={handleControls}
+                    >
+                        <ImageContainer image={image} />
+                    </Pressable>
+                    
+                    <Animated.View
+                        style={[{
+                            position: 'absolute',
+                            top: 10,
+                            right: 10,
+                            display: controlsVisible ? 'block' : 'none'
+                        }, animatedStyle]}
+                    >
+                        <IconButtonLarge
+                            name='close'
+                            onPress={closeImagesModal}
+                            size={40}
+                            color='tomato'
+                            transparent
+                        />
+                    </Animated.View>
 
-            <Animated.View
-                style={[{
-                    position: 'absolute',
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    zIndex: 100,
-                    backgroundColor: 'rgba(0, 0, 0, 0.6)',
-                }, animatedStyle]}
-            >
-                <ImageControlPanel image={image} />
-            </Animated.View>
-            
+                    <Animated.View
+                        style={[{
+                            position: 'absolute',
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            zIndex: 100,
+                            backgroundColor: 'rgba(0, 0, 0, 0.6)',
+                            paddingVertical: 10,
+                            display: controlsVisible ? 'block' : 'none',
+                        }, animatedStyle]}
+                    >
+                        <View style={{ paddingHorizontal: 10 }}>
+                            <ThemedText color='#fff'>{image.caption}</ThemedText>
+                        </View>
+                        <ImageControlPanel image={image} />
+                    </Animated.View>
+                    
+                </View>
+            ) : <ActivityIndicator />}
         </View>
-    ) : <ActivityIndicator />
+    )
 }
 
 export default UserImageDisplay
